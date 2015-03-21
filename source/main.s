d5000003e65c7e24447740bfbf079d461972c300
smlevorse <smlevorse@msn.com>
2015-03-21 18:24:32 -0400

    Lesson 1

--- source/main.s
+++ source/main.s
@@ -1,3 +1,15 @@
 .section .init
 .globl _start
 _start:
+
+ldr r0,=0x20200000 ; GPIO controller
+mov r1,#1				
+lsl r1,#18
+str r1,[r0,#4]
+
+mov r1,#1
+lsl r1,#16
+str r1,[r0,#40] 			; Turns on GPIO16, #28 turns it off
+
+loop$:
+b loop$
