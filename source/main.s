584686c1fbb84944d53dba7e0208975a48de2c41
smlevorse <smlevorse@msn.com>
2015-03-21 21:16:52 -0400

    lesson 2

--- source/main.s
+++ source/main.s
@@ -2,14 +2,31 @@
 .globl _start
 _start:
 
-ldr r0,=0x20200000 ; GPIO controller
+ldr r0,=0x20200000
 mov r1,#1				
 lsl r1,#18
 str r1,[r0,#4]
 
 mov r1,#1
 lsl r1,#16
-str r1,[r0,#40] 			; Turns on GPIO16, #28 turns it off
+start$:
+str r1,[r0,#40] 
+
+mov r2,#0x3F0000
+wait1$:
+sub r2,#1
+cmp r2,#0
+bne wait1$
+
+str r1,[r0,#28]
+
+mov r2,#0x3F0000
+wait2$:
+sub r2,#1
+cmp r2,#0
+bne wait2$
+
+b start$
 
 loop$:
 b loop$
