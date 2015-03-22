f65b43f394720c14e24ccff926eef4404ad45889
smlevorse <smlevorse@msn.com>
2015-03-22 18:09:37 -0400

    Lesson 3

--- source/main.s
+++ source/main.s
@@ -2,31 +2,52 @@
 .globl _start
 _start:
 
-ldr r0,=0x20200000
-mov r1,#1				
-lsl r1,#18
-str r1,[r0,#4]
+b main
 
-mov r1,#1
-lsl r1,#16
-start$:
-str r1,[r0,#40] 
+.section .text
+main:
+mov sp,#0x8000
 
-mov r2,#0x3F0000
+pinNum .req r0
+pinFunc .req r1
+mov pinNum,#16
+mov pinFunc,#1
+bl SetGpioFunction
+.unreq pinNum
+.unreq pinFunc
+
+loop$
+
+pinNum .req r0
+pinVal .req r1
+mov pinNum,#16
+mov pinVal,#0
+bl SetGpio
+.unreq pinNum
+.unreq pinVal
+
+.decr .req r0
+mov decr,#0x3F0000
 wait1$:
-sub r2,#1
-cmp r2,#0
-bne wait1$
+	sub decr,#1
+	teq decr,#0
+	bne wait1$
+.unreq decr
 
-str r1,[r0,#28]
+pinNum .req r0
+pinVal .req r1
+mov pinNum #16
+mov pinVal,#1
+bl SetGpio
+.unreq pinNum
+.unreq pinVal
 
-mov r2,#0x3F0000
+.decr .req r0
+mov decr,#0x3F0000
 wait2$:
-sub r2,#1
-cmp r2,#0
-bne wait2$
+	sub decr,#1
+	teq decr,#0
+	bne wait2$
+.unreq decr
 
-b start$
-
-loop$:
 b loop$
